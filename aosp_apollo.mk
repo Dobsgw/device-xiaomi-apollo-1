90e26c27fa3469d06fdd0012375137574b35e1b1
Dobsgw <george.w.dobson@gmail.com>
2022-07-28 20:20:25 +0100

    apollo: bringup aosp

--- aosp_apollo.mk
+++ aosp_apollo.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
@@ -20,7 +20,7 @@ DEVICE_MAINTAINER := Dobsgw
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-PRODUCT_NAME := arrow_apollo
+PRODUCT_NAME := aosp_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
